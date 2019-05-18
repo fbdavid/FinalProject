7d16be6fb17ae53aae8c34695c158094ec1ce306
fbdavid <50179226+fbdavid@users.noreply.github.com>
2019-05-18 00:20:22 -0400

    Update Finalproject.py

--- Finalproject.py
+++ Finalproject.py
@@ -6,12 +6,12 @@ Assignment: Final Project
 import csv
 
 def main():
-    #create class for players
+    # creates class for player stats
     class Player_stats(): 
-
+    #stats used
         def __init__(self,Player,DRB,AST,STL,BLK,PTS):
             self.Player = (Player)
-            #Turn player stats to floats 
+    # all stats need to be floats to do player score eqaution
             self.DRB = float(DRB) 
             self.AST = float(AST)
             self.STL = float(STL)
@@ -23,10 +23,10 @@ def main():
                 self.DRB = 0.09
                 self.STK = 0.09
                 self.BLK = 0.09
-            #Equation for overall player rating/score.    
-            self.score = ((self.PTS * self.AST) / (self.DRB + self.STL + self.BLK)) 
+                # equation 
+            self.score = ((self.PTS * self.AST) / (self.DRB + self.STL + self.BLK)) #Equation for overall player rating/score/
 
-
+ # hold main code for function 
     class main():
         playerlist = []
         teamlist = []
@@ -36,9 +36,10 @@ def main():
         team2_rating = []
         total_2 = 0
         #Read the dataset
-        with open("NBA_PLAYERS.csv") as f:
+        with open("NBA_PLAYERS.csv") as f: 
             data = csv.reader(f)
             first = True
+            # skips first column in dataset so strings can be converted
             for row in data:
                 if first:
                     first = False
@@ -46,28 +47,25 @@ def main():
                 #Adds all players and their stats to a list    
                 playerlist.append(Player_stats(row[0],row[21],row[23],row[24],row[25],row[28])) 
 
-            #print(len(playerlist))
-            #for player in playerlist:
-                #print(player.Player + "   Score: " + str(player.score))
+            # does not allow each team to have more than or less than 5 players.
 
-        #Loop for creating the first team. 
-        while len(teamlist) < 5: 
+        while len(teamlist) < 5:
 
             inp = input("Enter the name of a player: ")
-                 
+
             matches = [] 
-            #if user inputs a word thats in list it will print the matches         
+          # shows players that matches user input 
             for player in playerlist:
                 if inp.lower() in player.Player.lower():
                     matches.append(player)
-
+# if length of match is greater than 1, print matches
             if len(matches) > 1:
                 print("We found the following matches... \n")
                 for match in matches:
                     print(match.Player)
                 print("Please re-enter the full name of the player you would" +\
                 "like to add to your team.\n")
-
+      # if len of match equals 1 print the stats.
             if len(matches) == 1:
                 print("Would you like to add " + matches[0].Player + " to your team?")
                 print("PTS: " + str(matches[0].PTS) + " AST: " + str(matches[0].AST) +
@@ -90,30 +88,27 @@ def main():
 
         print("Team one roster is filled, Team 2 it's your turn to pick")
         print(" Team rating:" + str(team1_rating))
-
+  # just the sum total in team rating list
         for ele in range(0, len(team1_rating)):
             total_1 = total_1 + team1_rating[ele]
         print(total_1)
-        
 
-        #Loop for creating the second team.
         while len(team2list) < 5:
 
             inp = input("Enter the name of a player: ")
-            #Every player with commonalities is added to this list.
             matches = [] 
-
+#Every player with commonalities is added to this list.
             for player in playerlist:
                 if inp.lower() in player.Player.lower():
                     matches.append(player)
-
+ # if length of match is greater than 1, print the following
             if len(matches) > 1:
                 print("We found the following matches... \n")
                 for match in matches:
                     print(match.Player)
                 print("Please re-enter the full name of the player you would" +\
                 "like to add to your team.\n")
-
+# if len of match equals 1 print the stats.
             if len(matches) == 1:
                 print("Would you like to add " + matches[0].Player + " to your team?")
                 print("PTS: " + str(matches[0].PTS) + " AST: " + str(matches[0].AST) +
@@ -121,28 +116,29 @@ def main():
                         " BLK: " + str(matches[0].BLK))
 
                 inp = input("Y/N?")
-                # If users enters Y, y, or "yes" player is added to team.
-                if inp == 'Y' or inp == 'y' or inp == 'yes': 
+                # if user says y,Y,or Y, add player to team list
+                if inp == 'Y' or inp == 'y' or inp == 'yes':
                     team2list.append(matches[0])
                     print(matches[0].Player + " has been added. \n")
                     print("Player rating is:" + str(matches[0].score))
                     team2_rating.append(matches[0].score)
-
+# if player is misspelled or used already
             else:
                 print("Okay, player wont be added, let's try again.\n")
-
+# if player length equals zero tell someone to try it again
             if len(matches) == 0:
                 print("Sorry, we didn't find any players by that name.\
                 Please try again... \n")
         print("Team 2, your roster is filled, simulation would be complete soon!")
         print("Team Rating:" + str(team2_rating))
-
+# just the sum total in team rating list
         for ele in range(0, len(team2_rating)):
             total_2 = total_2 + team2_rating[ele]
         print(total_2)
-# takes both teams totals and chooses a winner based on the total team ratings
+# how we determine who the winner is 
         if total_1 > total_2:
             print("TEAM 1 WINS!!")
         else:
             print("TEAM 2 WINS!!")
+
 
